330580604f03118442e6e2d7de6b12ea9205d1b9
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-07-08 22:14:56 +0300

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import HeroSection from './sections/HeroSection';
-import PhoneModel from './sections/PhoneModel';
-import Quote from './sections/Quote';
-import { GlobalStyle } from './styles/GlobalStyle';
-
+import DesignSection from "./sections/DesignSection";
+import DisplaySection from "./sections/DisplaySection";
+import HeroSection from "./sections/HeroSection";
+import PhoneModel from "./sections/PhoneModel";
+import Quote from "./sections/Quote";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import ProcessorSection from "./sections/ProcessorSection";
+import BatterySection from "./sections/BatterySection";
+import ColorSection from "./sections/ColorSection";
+import CameraSection from "./sections/CameraSection";
+import PricingSection from "./sections/PricingSection";
+import { ColorContextProvider } from "./context/ColorContext";
 
 function App() {
   return (
@@ -11,6 +18,15 @@ function App() {
       <Quote />
       <PhoneModel />
       <HeroSection />
+      <DesignSection />
+      <DisplaySection />
+      <ProcessorSection />
+      <BatterySection />
+      <ColorContextProvider>
+        <ColorSection />
+        <CameraSection />
+        <PricingSection />
+      </ColorContextProvider>
     </>
   );
 }
